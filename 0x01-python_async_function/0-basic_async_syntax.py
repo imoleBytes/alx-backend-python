e7e7f680224717fb56387d98797816a0904f151a
imoleFolio <dremkay71@gmail.com>
2024-08-05 13:59:40 +0100

    tak 0: uniform returned, all example code removed

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -13,10 +13,6 @@ import random
 
 async def wait_random(max_delay: int = 10) -> float:
     """wait a random a certain amountof time between 0 and max_delay"""
-    delay: float = random.random() * max_delay
+    delay: float = random.uniform(0, max_delay)
     await asyncio.sleep(delay)
     return delay
-
-print(asyncio.run(wait_random()))
-print(asyncio.run(wait_random(5)))
-print(asyncio.run(wait_random(15)))
